d16535348401fd51c5b94293ffeb86657bd6a75f
Hamed Rezaee <13097270+hamed-rezaee@users.noreply.github.com>
2020-07-11 13:18:12 +0430

    - initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_cubit_todo/home_page.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Cubit',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Home Page'),
+    );
+  }
+}
